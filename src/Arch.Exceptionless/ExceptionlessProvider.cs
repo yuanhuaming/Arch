1daaade76fe5b382186c82cf324329cc077e9006
KC\yuanhuaming <yuanhuaming@msn.com>
2018-01-12 18:32:18 +0800

    增加exceptionless调用方法

---
+++ src/Arch.Exceptionless/ExceptionlessProvider.cs
@@ -0,0 +1,13 @@
+﻿using Exceptionless;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arch.Exceptionless
+{
+    public class ExceptionlessProvider
+    {
+
+
+    }
+}
